089a315f933020a821e130aa11db160e3cf02b41
Momona Nakagawa <105970085@student.swin.edu.au>
2026-09-12 21:13:22 +0800

    Complete Exercise 4.2

--- Exercise 4/Exercise 4.1/assets/js/main.js
+++ Exercise 4/Exercise 4.1/assets/js/main.js
@@ -1,12 +1,14 @@
-const svg = d3.select(".responsive-svg-container")
-    .append("svg")
-    .attr("viewBox", "0 0 1200 1600")
-    .style("border", "1px solid black");
+d3.select("h1")
+  .style("color", "green");
 
-svg
+  d3.select("div")
+  .append("p")
+    .text("Purchasing a low energy consumption TV will help with your energy bills!");
+
+d3.select("svg")
   .append("rect")
-    .attr("x", 10)
-    .attr("y", 10)
-    .attr("width", 414)
-    .attr("height", 16)
-    .attr("fill", "blue");
+  .attr("x", 50)
+   .attr("y", 50)
+   .attr("width", 100)
+   .attr("height", 30)
+   .style("fill", "green");
